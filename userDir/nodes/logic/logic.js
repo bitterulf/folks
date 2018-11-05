13a0addc3cf505b0e11731eb8b48856d2aaca149
Sebastian Kannegiesser <s.kannegiesser@xpure.de>
2018-11-05 15:04:22 +0100

    Add seneca

--- userDir/nodes/logic/logic.js
+++ userDir/nodes/logic/logic.js
@@ -1,11 +1,26 @@
+const seneca = require('seneca')()
+
+seneca.add('command:web,page:status', (msg, reply) => {
+    reply(null, {
+        template: 'status',
+        status: 'OK'
+    });
+})
+
 module.exports = function(RED) {
     function Node(config) {
         RED.nodes.createNode(this,config);
         var node = this;
 
         node.on('input', function(msg) {
-            msg.payload = msg.payload + 'foo';
-            node.send(msg);
+            seneca.act(msg.payload, function (err, result) {
+                if (err) {
+                    msg.payload = { error: err.message };
+                    return node.send([null, msg]);
+                }
+                msg.payload = result;
+                node.send([msg, null]);
+            })
         });
     }
     RED.nodes.registerType("logic", Node);
